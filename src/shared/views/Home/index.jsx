6527d6cefcde3474ddd892e212586ebce050a23e
przekwas <luke@covalence.io>
2021-05-21 17:00:16 -0500

    changes

--- src/shared/views/Home/index.jsx
+++ src/shared/views/Home/index.jsx
@@ -5,7 +5,9 @@ function Home() {
 	return (
 		<BasePage>
 			<div className="flex items-center justify-between w-full text-4xl">
-				<GiCrane /> <span>a work in progress lmao</span> <GiCrane />
+				<GiCrane />
+				<span className="text-center">a proof of concept in progress lmao</span>
+				<GiCrane />
 			</div>
 		</BasePage>
 	);
