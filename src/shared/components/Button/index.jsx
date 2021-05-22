e3ae23633c6747a963f3dc6c737891a7ba7f211a
przekwas <luke@covalence.io>
2021-05-21 23:38:13 -0500

    changes

--- src/shared/components/Button/index.jsx
+++ src/shared/components/Button/index.jsx
@@ -1,7 +1,37 @@
 function Button({ color, className, onClick, children }) {
-	const type = `btn-${color}`;
+	let type = null;
+	switch (color) {
+		case 'purple':
+			type = 'bg-purple-500 hover:bg-purple-700 focus:ring-purple-400';
+			break;
+		case 'indigo':
+			type = 'bg-indigo-500 hover:bg-indigo-700 focus:ring-indigo-400';
+			break;
+		case 'blue':
+			type = 'bg-blue-500 hover:bg-blue-700 focus:ring-blue-400';
+			break;
+		case 'pink':
+			type = 'bg-pink-500 hover:bg-pink-700 focus:ring-pink-400';
+			break;
+		case 'gray':
+			type = 'bg-gray-500 hover:bg-gray-700 focus:ring-gray-400';
+			break;
+		case 'red':
+			type = 'bg-red-500 hover:bg-red-700 focus:ring-red-400';
+			break;
+		case 'yellow':
+			type = 'bg-yellow-500 hover:bg-yellow-700 focus:ring-yellow-400';
+			break;
+		case 'green':
+			type = 'bg-green-500 hover:bg-green-700 focus:ring-green-400';
+			break;
+		default:
+			break;
+	}
 	return (
-		<button className={`${type} ${className}`} onClick={onClick}>
+		<button
+			className={`px-4 py-2 font-semibold rounded-lg shadow-md text-white focus:outline-none focus:ring-2 focus:ring-opacity-75 ${type} ${className}`}
+			onClick={onClick}>
 			{children}
 		</button>
 	);
